f73baddccb9a1cd298ab25686fa4303600bb69b1
KitsuneDai <149829535+KitsuneDai@users.noreply.github.com>
2023-12-10 17:45:13 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! :)");
+};
+
+let generateExcuse = () => {
+  let pronombre = ["A", "The"];
+  let sujeto = ["jogger", "dog", "driver", "comedian", "pincone"];
+  let accion = ["took my", "eat my", "yelled at my", "stole my", "bite my"];
+  let posesion = ["toe", "homework", "car", "hat"];
+  let lugar = ["on the street", "in my house", "in my driveway"];
+
+  let pronIndx = Math.floor(Math.random() * pronombre.length);
+  let suIndx = Math.floor(Math.random() * sujeto.length);
+  let accIndx = Math.floor(Math.random() * accion.length);
+  let poseIndx = Math.floor(Math.random() * posesion.length);
+  let lugIndx = Math.floor(Math.random() * lugar.length);
+
+  return (
+    pronombre[pronIndx] +
+    " " +
+    sujeto[suIndx] +
+    " " +
+    accion[accIndx] +
+    " " +
+    posesion[poseIndx] +
+    " " +
+    lugar[lugIndx] +
+    "."
+  );
 };
